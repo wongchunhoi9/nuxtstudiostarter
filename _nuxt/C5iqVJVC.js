ef71e73caec5c774dd085d3cfc9637eb8cab6156
wongchunhoi9 <57960522+wongchunhoi9@users.noreply.github.com>
2025-03-25 15:48:24 +0000

    Deploying to gh-pages from @ wongchunhoi9/nuxtstudiostarter@840757ee095bc99428d6ba6a652822220a2b291a 🚀

---
+++ _nuxt/C5iqVJVC.js
@@ -0,0 +1,1 @@
+import{l as o,f as s,k as r,W as t}from"./DMrk-Gbu.js";const n={};function a(e,c){return r(),s("tbody",null,[t(e.$slots,"default")])}const _=Object.assign(o(n,[["render",a]]),{__name:"ProseTbody"});export{_ as default};
