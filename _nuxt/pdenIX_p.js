79f096ef2537a16bdd90764d748e5330491dfff9
wongchunhoi9 <57960522+wongchunhoi9@users.noreply.github.com>
2025-04-01 16:41:18 +0000

    Deploying to gh-pages from @ wongchunhoi9/nuxtstudiostarter@fe7eaf534510afbedb06a088f149810d3e83b6ee 🚀

---
+++ _nuxt/pdenIX_p.js
@@ -0,0 +1,1 @@
+import{l as s,f as r,k as t,W as n}from"./CqFdH3o0.js";const o={};function a(e,c){return t(),r("td",null,[n(e.$slots,"default")])}const _=Object.assign(s(o,[["render",a]]),{__name:"ProseTd"});export{_ as default};
