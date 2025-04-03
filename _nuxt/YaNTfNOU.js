9e2a2cba60830465c85a07d7d95c3978418e4925
wongchunhoi9 <57960522+wongchunhoi9@users.noreply.github.com>
2025-04-03 08:29:53 +0000

    Deploying to gh-pages from @ wongchunhoi9/nuxtstudiostarter@410fb419c0cc979ff635497e571b23587c093b80 🚀

--- _nuxt/YaNTfNOU.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./CY0i3djy.js";import{l as n,C as r,k as t,a8 as a}from"./CkCE548o.js";const c={};function _(s,m){const e=o;return t(),r(e,a(s.$attrs,{class:"p-2",loading:"lazy",fit:"cover",sizes:"xs:320px md:768px lg:1024px xl:1280px"}),null,16)}const x=Object.assign(n(c,[["render",_]]),{__name:"ProseImg"});export{x as default};
