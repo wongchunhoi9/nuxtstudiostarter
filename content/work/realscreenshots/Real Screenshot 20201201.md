74a42108b17c2c17ab42523d881ca74d9d939b3b
wongchunhoi9 <57960522+wongchunhoi9@users.noreply.github.com>
2025-04-28 11:21:48 +0800

    real screen shot building

---
+++ content/work/realscreenshots/Real Screenshot 20201201.md
@@ -0,0 +1,16 @@
+---
+title: Real Screenshot 20201201
+description: 'a series of images captured when i saw broken screen in city'
+date: 2020-12-01 
+time: 20:57:14 
+location: Fortress, New Town Plaza, Sha Tin, Hong Kong 
+tags: [image, tag2, tag3, tag4]
+category: [Image, electronics]
+img: /img/realScreenShot/20201201-205714.jpg
+slug: asbdad
+---
+
+ ![Alttext](/img/realScreenShot/20201201-205714.jpg)  
+
+2020-12-01    20:57:14  
+Fortress, New Town Plaza, Sha Tin, Hong Kong 
--- pages/work/RealScreenShots/[slug].vue
+++ pages/work/RealScreenShots/[slug].vue
@@ -6,13 +6,32 @@ const { data: post } = await useAsyncData(`realscreenshots-${slug}`, () => {
 
 console.log('post', post)
 
+// Fetch surrounding blog posts (previous and next)
+const { data: surroundings } = await useAsyncData(`realscreenshots-surroundings-${slug}`, () => {
+  return queryCollectionItemSurroundings('RealScreenShots', `/work/realscreenshots/${slug}`)
+})
+
 </script>
 
 
 
 <template>
-  <div class="container mx-auto md:px-4">
+  <div class="container mx-auto md:px-4 max-w-4xl">
     <!-- Render the blog post as Prose & Vue components -->
     <ContentRenderer :value="post" />
+  </div>
+
+    <!-- Surrounding Blog Posts -->
+    <div class="mt-8 flex justify-between">
+    <!-- Previous Post -->
+    <NuxtLink v-if="surroundings?.[0]" :to="surroundings[0].path" class="text-blue-500 hover:underline">
+      ← {{ surroundings[0].title }}
+    </NuxtLink>
+  
+
+    <!-- Next Post -->
+    <NuxtLink v-if="surroundings?.[1]" :to="surroundings[1].path" class="text-blue-500 hover:underline">
+      {{ surroundings[1].title }} →
+    </NuxtLink>
   </div>
 </template>
