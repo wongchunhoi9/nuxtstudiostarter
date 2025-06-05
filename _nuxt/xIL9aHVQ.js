dcfbf7d1481a23cc3c579ee9047a4e31a1476fc8
wongchunhoi9 <57960522+wongchunhoi9@users.noreply.github.com>
2025-06-05 14:26:41 +0000

    Deploying to gh-pages from @ wongchunhoi9/nuxtstudiostarter@b32ffc01f9eb654273b24c0890693af261485e2d 🚀

--- _nuxt/xIL9aHVQ.js
+++
@@ -1,1 +0,0 @@
-import{_ as s}from"./DlAUqK2U.js";import{e as o,f as t,a2 as r}from"./B0qKN9yn.js";const a={},c={class:"font-semibold italic"};function n(e,_){return t(),o("em",c,[r(e.$slots,"default")])}const l=Object.assign(s(a,[["render",n]]),{__name:"ProseEm"});export{l as default};
